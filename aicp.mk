e3507204fc53335b53975ef84e88a26349843b3d
dkati <dkatikaridis@gmail.com>
2018-07-12 12:04:19 +0300

    aicp.mk:Reenable our vendor configs
    Change-Id: I865e9466a18d65358bc6dd86a839afba69bcf079

--- aicp.mk
+++ aicp.mk
@@ -3,7 +3,7 @@ $(call inherit-product, device/samsung/jflte/full_jflte.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/aicp/config/common_full_phone.mk)
 
-# Inherit some common JDC stuff.
-#$(call inherit-product, vendor/jdc/config/common.mk)
+# Inherit some common Sausage stuff.
+$(call inherit-product, vendor/barbeque/config/common.mk)
 
 PRODUCT_NAME := aicp_jflte
